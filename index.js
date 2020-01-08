76ec3cae1aca3d37191ef14c5ec65c418d340625
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-08 04:38:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 
@@ -33,7 +33,3 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
 }
-
-
-
-
